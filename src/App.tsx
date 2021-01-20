ac0460a2435cc766012cdb9b071060c547c82379
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-01-20 20:15:33 +0100

    Half-working dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,15 @@ import {
   Card,
   Divider,
   Grid,
+  Switch,
+  ThemeProvider,
+  Container,
 } from "@material-ui/core";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createStyles,
+  createMuiTheme,
+} from "@material-ui/core/styles";
 import { ArrowUpward, ArrowDownward, ImportExport } from "@material-ui/icons";
 
 const useStyles = makeStyles(() =>
@@ -31,74 +38,100 @@ const useStyles = makeStyles(() =>
     madeBy: {
       margin: 10,
     },
+    darkMode: {
+      margin: 10,
+    },
   })
 );
 
 function App() {
   const [repeaterCount, setRepeaterCount] = useState(1);
   const classes = useStyles();
+  const [darkState, setDarkState] = useState(true);
+  const palletType = darkState ? "dark" : "light";
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: palletType,
+    },
+  });
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
 
   return (
-    <div className="App">
-      <Card className={classes.card}>
-        <Grid container spacing={2}>
-          <Grid item sm={6} xs={12} className={classes.flexCenter}>
-            <Typography variant="body1">
-              <Typography variant="h5">The Repeater Calculator</Typography>
-            </Typography>
-          </Grid>
-          <Grid item sm={3} xs={6} className={classes.flexCenter}>
-            <Typography variant="body1">
-              {repeaterCount * 1} repeater{repeaterCount > 1 ? "s" : ""}
-            </Typography>
-          </Grid>
-          <Grid item sm={3} xs={6} className={classes.flexCenter}>
-            <ButtonGroup variant="contained">
-              <Button onClick={() => addCount(1)}>
-                <ArrowUpward />
-              </Button>
-              <Button onClick={() => addCount(-1)}>
-                <ArrowDownward />
-              </Button>
-              <Button onClick={textBoxInput}>
-                <ImportExport />
-              </Button>
-            </ButtonGroup>
-          </Grid>
-        </Grid>
-        <Divider className={classes.divider} />
-        <Grid container spacing={2}>
-          <Grid item className={classes.bottomSection}>
-            <Typography variant="body1">Final ingredients</Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 3)} stone
-            </Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 2)} redstone torches
-            </Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 1)} redstone dust
-            </Typography>
-          </Grid>
-          <Grid item className={classes.bottomSection}>
-            <Typography variant="body1">Raw ingredients</Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 3)} stone
-            </Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 1)} wooden plank
-              {repeaterCount > 1 ? "s" : ""}
-            </Typography>
-            <Typography variant="body1">
-              {prettyStacks(repeaterCount * 3)} redstone dust
-            </Typography>
-          </Grid>
-        </Grid>
-      </Card>
-      <Typography variant="body1" className={classes.madeBy}>
-        Made by Honbra <br /> Special thanks to Tokfrans03 for the item displays
-      </Typography>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <div className="App">
+          <Card className={classes.card}>
+            <Grid container spacing={2}>
+              <Grid item sm={6} xs={12} className={classes.flexCenter}>
+                <Typography variant="body1">
+                  <Typography variant="h5">The Repeater Calculator</Typography>
+                </Typography>
+              </Grid>
+              <Grid item sm={3} xs={6} className={classes.flexCenter}>
+                <Typography variant="body1">
+                  {repeaterCount * 1} repeater{repeaterCount > 1 ? "s" : ""}
+                </Typography>
+              </Grid>
+              <Grid item sm={3} xs={6} className={classes.flexCenter}>
+                <ButtonGroup variant="contained">
+                  <Button onClick={() => addCount(1)}>
+                    <ArrowUpward />
+                  </Button>
+                  <Button onClick={() => addCount(-1)}>
+                    <ArrowDownward />
+                  </Button>
+                  <Button onClick={textBoxInput}>
+                    <ImportExport />
+                  </Button>
+                </ButtonGroup>
+              </Grid>
+            </Grid>
+            <Divider className={classes.divider} />
+            <Grid container spacing={2}>
+              <Grid item className={classes.bottomSection}>
+                <Typography variant="body1">Final ingredients</Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 3)} stone
+                </Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 2)} redstone torches
+                </Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 1)} redstone dust
+                </Typography>
+              </Grid>
+              <Grid item className={classes.bottomSection}>
+                <Typography variant="body1">Raw ingredients</Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 3)} stone
+                </Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 1)} wooden plank
+                  {repeaterCount > 1 ? "s" : ""}
+                </Typography>
+                <Typography variant="body1">
+                  {prettyStacks(repeaterCount * 3)} redstone dust
+                </Typography>
+              </Grid>
+            </Grid>
+          </Card>
+          <div className={classes.darkMode}>
+            <label htmlFor="darkmode">Dark mode (broken)</label>
+            <Switch
+              checked={darkState}
+              onChange={handleThemeChange}
+              id="darkmode"
+            />
+          </div>
+          <Typography variant="body1" className={classes.madeBy}>
+            Made by Honbra <br /> Special thanks to Tokfrans03 for the item
+            displays
+          </Typography>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 
   function addCount(i: number) {
